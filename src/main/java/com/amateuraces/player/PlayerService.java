f37d06fe45cb41a517d2f94b80425be95b14416f
John Doe <johndoe@example.com>
2024-10-02 18:46:16 +0800

    Resolved Player Bug
    Delete any changes you have made, then pull this

--- src/main/java/com/amateuraces/player/PlayerService.java
+++ src/main/java/com/amateuraces/player/PlayerService.java
@@ -1,32 +1,42 @@
 package com.amateuraces.player;
+
 import java.util.List;
 
 public interface PlayerService {
+    /**
+     * Retrieve all players.
+     * @return List of players.
+     */
     List<Player> listPlayers();
 
+    /**
+     * Retrieve a player by ID.
+     * @param id the player's ID
+     * @return the player
+     */
     Player getPlayer(Long id);
 
     /**
-     * Return the newly added player
+     * Return the newly added player.
+     * @param player the player to add
+     * @return the added player
      */
     Player addPlayer(Player player);
 
     /**
-     * Return the updated player
-     * 
-     * @param id
-     * @param player
-     * @return
+     * Return the updated player.
+     * @param id the player ID
+     * @param player the updated player details
+     * @return the updated player
      */
     Player updatePlayer(Long id, Player player);
 
     /**
-     * Return status of the delete
-     * If it's 1: the player has been removed
-     * If it's 0: the player does not exist
-     * 
-     * @param id
-     * @return
+     * Return status of the delete.
+     * If it's 1: the player has been removed.
+     * If it's 0: the player does not exist.
+     * @param id the player ID
+     * @return status of the delete operation
      */
     int deletePlayer(Long id);
 }
