ed61e8bfde6d1d998159111a1d1c33c17b8f7101
BingyBR <bingren.lee.2023@scis.smu.edu.sg>
2024-10-02 16:17:45 +0800

    test

--- project/src/main/java/com/amateuraces/project/services/TournamentService.java
+++ project/src/main/java/com/amateuraces/project/services/TournamentService.java
@@ -1,15 +1,11 @@
 package com.amateuraces.project.services;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.amateuraces.project.entity.Tournament;
+import com.amateuraces.project.repository.PlayerRepository;
+import com.amateuraces.project.repository.TournamentRepository;
 
-public interface TournamentService {
-    // void createTournament(TournamentDTO tournamentDTO);
-    // void updateTournament(TournamentDTO tournamentDTO);
-    void deleteTournament(String tournamentId);
-    List<Tournament> getAllTournaments();
-    Tournament getTournamentById(String tournamentId);
-    void registerPlayer(String tournamentId, String playerId);
-    void organizeMatches(String tournamentId);
-}
+public interface TournamentService {}
