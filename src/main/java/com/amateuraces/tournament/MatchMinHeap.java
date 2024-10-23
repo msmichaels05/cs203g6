d9b82de4298e421c16f15a1f31af8bebae7aec93
JadenTyh <jadentyh@gmail.com>
2024-10-24 01:37:20 +0800

    Started creating algorithm to create draw for tournament

---
+++ src/main/java/com/amateuraces/tournament/MatchMinHeap.java
@@ -0,0 +1,150 @@
+package com.amateuraces.tournament;
+
+import com.amateuraces.match.Match;
+//import org.hibernate.mapping.List;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MatchMinHeap {
+    private Match[] heap;
+    private int size;
+
+    public MatchMinHeap(int size) {
+        this.heap = new Match[size];
+        this.size = 0;
+    }
+
+    // Get the parent index
+    private int getParentIndex(int index) { //root is the final match
+        return (index - 1) / 2; 
+    }
+
+    // Get the left child index
+    private int getLeftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    // Get the right child index
+    private int getRightChildIndex(int index) {
+        return 2 * index + 2;
+    }
+
+    // Swap two elements in the heap
+    private void swap(int i, int j) {
+        Match temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    // Double the size of the array if it's full
+    private void resize() {
+        Match[] newHeap = new Match[heap.length * 2];
+        System.arraycopy(heap, 0, newHeap, 0, heap.length);
+        heap = newHeap;
+    }
+
+    // Add a new match to the heap
+    public void insert(Match match) {
+        if (size == heap.length) {
+            resize();  // Resize the array if it's full
+        }
+        heap[size] = match;  // Add the new match to the end of the heap
+        size++;
+        heapifyUp(size - 1);  // Restore the heap property by moving the match up
+    }
+
+    // Remove the root (smallest matchId) from the heap
+    public Match remove() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+
+        Match root = heap[0];
+        heap[0] = heap[size - 1];  // Move the last match to the root
+        size--;
+        heapifyDown(0);  // Restore the heap property by moving the root down
+
+        return root;  // Return the match with the smallest matchId
+    }
+
+    // Restore the heap property by moving the match at index up
+    private void heapifyUp(int index) {
+        int parentIndex = getParentIndex(index);
+
+        while (index > 0 && heap[index].getId() < heap[parentIndex].getId()) {
+            swap(index, parentIndex);  // Swap with the parent if the current matchId is smaller
+            index = parentIndex;
+            parentIndex = getParentIndex(index);
+        }
+    }
+
+    // Restore the heap property by moving the match at index down
+    private void heapifyDown(int index) {
+        int leftChildIndex = getLeftChildIndex(index);
+        int rightChildIndex = getRightChildIndex(index);
+        int smallestIndex = index;
+
+        if (leftChildIndex < size && heap[leftChildIndex].getId() < heap[smallestIndex].getId()) {
+            smallestIndex = leftChildIndex;
+        }
+
+        if (rightChildIndex < size && heap[rightChildIndex].getId() < heap[smallestIndex].getId()) {
+            smallestIndex = rightChildIndex;
+        }
+
+        if (smallestIndex != index) {
+            swap(index, smallestIndex);
+            heapifyDown(smallestIndex);  // Recursively heapify down
+        }
+    }
+
+    // Get the size of the heap
+    public int size() {
+        return size;
+    }
+
+    // Check if the heap is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Peek the root (smallest matchId) without removing it
+    public Match peek() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        return heap[0];
+    }
+
+    // Method to get the height of the heap
+    private int getHeight() {
+        if (size == 0) {
+            return -1; // Empty heap has height -1
+        }
+        return (int) (Math.floor(Math.log(size) / Math.log(2)));
+        // Alternatively, use the bitwise method:
+        // int height = -1;
+        // int nodes = size;
+        // while (nodes > 0) {
+        //     nodes = nodes >> 1;
+        //     height++;
+        // }
+        // return height;
+    }
+
+    public List<Match> matchesInRound(int roundNo) {
+        List<Match> nodesAtHeight = new ArrayList<>();
+        if (getHeight() == -1) return nodesAtHeight; // If draw is not out yet / deadline for registration not up yet
+        int h = getHeight() - roundNo + 1;
+        int startIndex = (1 << h) - 1;             // Calculate starting index
+        int endIndex = (1 << (h + 1)) - 2;         // Calculate ending index
+
+        // Ensure indices are within the bounds of the heap size
+        for (int i = startIndex; i <= endIndex && i < size; i++) {
+            nodesAtHeight.add(heap[i]);
+        }
+
+        return nodesAtHeight;
+    }
+}
+
--- src/main/java/com/amateuraces/tournament/Tournament.java
+++ src/main/java/com/amateuraces/tournament/Tournament.java
@@ -1,20 +1,25 @@
 package com.amateuraces.tournament;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.amateuraces.player.Player;
+import com.amateuraces.match.Match;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+//import org.hibernate.mapping.List;
+
+import com.amateuraces.player.Player;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 
 @Entity
 @Getter
@@ -22,7 +27,6 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
 public class Tournament {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,14 +36,186 @@ public class Tournament {
     @Size(min = 1, max = 100)
     private String name;
 
-    private int requirement;
+    private int ELOrequirement;
 
-    // Constructor with wins and losses
-    public Tournament(String name, int requirement) {
-        this.requirement = requirement;
+    private int maxPlayers;
+
+    private int playerCount = 0;
+
+    private LocalDate startDate;
+    
+    private LocalDate endDate;
+
+    private String location;
+
+    private String description;
+
+    private MatchMinHeap draw = null; //need to determine size eventually
+
+    @ManyToMany
+    @JoinTable(
+        name = "tournament_players",
+        joinColumns = @JoinColumn(name = "tournament_id"),
+        inverseJoinColumns = @JoinColumn(name = "player_id")
+    )
+    @JsonIgnore
+    private Set<Player> players = new HashSet<>();
+
+        // // Implement equals and hashCode
+        // @Override
+        // public boolean equals(Object o) {
+        //     if (this == o) return true;
+        //     if (!(o instanceof Tournament)) return false;
+        //     Tournament tournament = (Tournament) o;
+        //     return Objects.equals(id, tournament.id); // Compare based on 'id'
+        // }
+    
+        // @Override
+        // public int hashCode() {
+        //     return Objects.hash(id); // Hash based on 'id'
+        // }
+
+
+    public Tournament(String name, int maxPlayers, String location, int ELOrequirement, String description) {
+        this.ELOrequirement = ELOrequirement;
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+        this.location = location;
+        this.description = description;
+        this.draw = new MatchMinHeap(maxPlayers);
+    }
+
+    public Tournament(String name) {
         this.name = name;
     }
 
+    public Tournament(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public boolean addPlayer(Player player) {
+        boolean added = players.add(player);
+        if (added) {
+            playerCount++;
+            player.getTournaments().add(this); // Ensure the bi-directional relationship is maintained
+        }
+        return added;
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+        player.getTournaments().remove(this); // Also remove this tournament from the player's list
+    }
+    // public boolean addPlayer(Player player){
+    //     players.add(player);
+    //     return true;
+    // }
+
+    // // Get the count of registered players
+    // public int getRegisteredPlayerCount() {
+    //     return players.size();
+    // }
+
+    // // Clear the list of players
+    // public void clearPlayers() {
+    //     players.clear();
+    // }
+
+    public void initialiseDraw() { //Algorithm to create the draw
+        //Once there are 17 players, there has to be 16 1st round matches, only 2 unlucky players have to face off in 1st round, everybody else gets BYE in the 1st round
+        int round1Matches = (int) Math.pow(2, (int) Math.ceil((Math.log(playerCount) / Math.log(2))) -1); //Get total number of 1st round matches, even matches with BYES
+        int totalRounds = (int) Math.log((double)round1Matches); //total number of rounds excluding 1st round
+        for (int i=1; i<totalRounds; i*=2) {
+            for (int j=0; j<i; j++) { //create match for each round, start from finals -> 2nd round
+                Match tempmatch = new Match(null, null);  //Since subsequent matches outcome not determined yet, players are set to null 
+                draw.insert(tempmatch); //insert match for the particular round
+            }
+        } //All matches for subsequent rounds have been created and added to the drawHeap
+
+        List<Player> seededPlayers = new ArrayList<>();
+        List<Player> unseededPlayers = new ArrayList<>();
+        //Player[] slots = tournamentrepo;
+        assignSeedsToSlots(seededPlayers, null);
+        for (int r=0; r<round1Matches; r++) {  //1st match 1st seed, last match 2nd seed, n/2 match 3rd seed, n/2 + 1 4th seed
+            Match temp1stRoundMatch = null;
+            if (r==0) {//Assign player of highest elo (highest seed) to first match
+                //temp1stRoundMatch = new Match(seededPlayers[0], getRandomUnseededPlayer) ;
+            }
+            if (r==round1Matches - 1) {
+                //temp1stRoundMatch = new Match(getRandomUnseededPlayer, seededPlayers[seededPlayers.length-1]);
+            }
+            else {
+                //temp1stRoundMatch = new Match(getRandomUnseededPlayer, getRandomUnseededPlayer);
+            }
+            draw.insert(temp1stRoundMatch);
+        }
+    }
+
+    // Method to assign seeded players to specific slots
+    private static void assignSeedsToSlots(List<Player> seededPlayers, Player[] slots) {
+        int totalMatches = slots.length / 2;
+
+        // Seed 1
+        slots[0] = seededPlayers.get(0);
+
+        // Seed 2
+        slots[slots.length - 1] = seededPlayers.get(1);
+
+        // Seeds 3 and 4
+        int midPoint = totalMatches / 2;
+        slots[midPoint - 1] = seededPlayers.get(2);
+        slots[midPoint] = seededPlayers.get(3);
+
+        // Seeds 5 to n
+        int seedIndex = 4;
+        int[] predefinedPositions = getSeedPositions(seededPlayers.size(), slots.length);
+
+        for (int pos : predefinedPositions) {
+            if (seedIndex >= seededPlayers.size()) break;
+            slots[pos] = seededPlayers.get(seedIndex);
+            seedIndex++;
+        }
+    }
+
+    // Method to get predefined positions for seeds 5 to n
+    private static int[] getSeedPositions(int numberOfSeeds, int totalSlots) {
+        // Positions can be predefined according to tournament standards
+        // For simplicity, we'll return an array of positions
+        // Adjust this method based on the specific seeding rules
+
+        // Example positions for seeds 5 to 8 in a 32-player draw
+        return new int[]{(totalSlots / 4) - 1, (3 * totalSlots / 4), (totalSlots / 4), (3 * totalSlots / 4) - 1};
+    }
+
+    // Method to fill remaining slots with unseeded players
+    private static void fillUnseededPlayers(Player[] slots, List<Player> unseededPlayers) {
+        Random rand = new Random();
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == null) {
+                int index = rand.nextInt(unseededPlayers.size());
+                slots[i] = unseededPlayers.remove(index);
+            }
+        }
+    }
+
+    // Placeholder methods to get seeded and unseeded players
+    private void seededUnseeded() { //Create list of seeded & unseeded players
+        int no = (int) Math.pow(2, (int) Math.ceil((Math.log(playerCount) / Math.log(2))) -1);
+        int numberOfSeeds = no / 4;
+        //TOBE IMPLEMENTEDDD
+        // //List<Player> playersByRank = descendingOrderByELO
+        // List<Player> seededPlayers = new ArrayList<>();
+        // List<Player> unseededPlayers = new ArrayList<>();
+        // //From tournament repo, get all playersInTournament()
+        // int i;
+        // for (i=0; i<numberOfSeeds; i++) {
+        //     seededPlayers.add(playersByRank.get(i));
+        // }
+        // for (int j=i; j<playersByRank.size(); j++) {
+        //     unseededPlayers.add(-playersByRank.get(j));
+        // }
+    }
 
 }
 
