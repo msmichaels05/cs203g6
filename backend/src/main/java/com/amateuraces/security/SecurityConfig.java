fdaf06ca29df70e4c759375e326e92c0f9a4b6bc
Chase Lim <chaselim96@gmail.com>
2024-11-19 17:04:32 +0800

    deploy 19

--- backend/src/main/java/com/amateuraces/security/SecurityConfig.java
+++ backend/src/main/java/com/amateuraces/security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow only React frontend URL
+    configuration.setAllowedOrigins(List.of("https://cs203g6-tennis.vercel.app")); // Allow only React frontend URL
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true); // Allow credentials such as cookies, authorization headers
